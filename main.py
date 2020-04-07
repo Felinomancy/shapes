62a11ffd9a75e3296995016aa0b56df0365a85f2
Amry <amryxx@gmail.com>
2020-04-08 00:23:41 +0800

    Testing the commit function

---
+++ main.py
@@ -0,0 +1,13 @@
+# main executable file
+
+
+def main():
+    print("This is a test")
+
+
+def someOtherFunction():
+    print("Whatever")
+
+
+if __name__ == '__main__':
+    main()
---
+++ shapes.py
@@ -0,0 +1,1 @@
+# defines the various shape classes
